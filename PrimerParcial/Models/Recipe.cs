410151509ba86d654b94b8909180d50da6746693
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-09-25 19:52:15 -0400

    AdrianLayme/4) Implementar el modelo Recipe en funcion a la migracion y las relaciones correspondientes con los modelos

--- PrimerParcial/Models/Recipe.cs
+++ PrimerParcial/Models/Recipe.cs
@@ -1,6 +1,36 @@
-﻿namespace PrimerParcial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
 {
     public class Recipe
     {
+        // Modelado del Recipe
+        public int Id { get; set; }
+
+        [Required] 
+        [StringLength(100)] 
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int PreparationTimeMinutes { get; set; }
+
+        [Required]
+        public int Servings { get; set; }
+        [Required] 
+        public string Instructions { get; set; } 
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+
+
     }
 }
